774ffd5a56abbc303273cb9f38d55169f757a09b
ShaerWare <artemsher@gmail.com>
2025-12-07 14:50:20 +0000

    +1

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,8 @@ Route::get('/', function () {
 });
 
 // Публичный доступ к списку и просмотру компаний
-Route::get('companies', [CompanyController::class, 'index']);
-Route::get('companies/{company}', [CompanyController::class, 'show']);----
+Route::get('companies', [CompanyController::class, 'index'])->name('companies.index');
+Route::get('companies/{company}', [CompanyController::class, 'show'])->name('companies.show');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -21,7 +21,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::apiResource('companies', CompanyController::class);
+    
+    // CRUD компаний (создание, обновление, удаление только для авторизованных)
+    Route::post('companies', [CompanyController::class, 'store'])->name('companies.store');
+    Route::get('companies/create', [CompanyController::class, 'create'])->name('companies.create');
+    Route::get('companies/{company}/edit', [CompanyController::class, 'edit'])->name('companies.edit');
+    Route::put('companies/{company}', [CompanyController::class, 'update'])->name('companies.update');
+    Route::delete('companies/{company}', [CompanyController::class, 'destroy'])->name('companies.destroy');
 });
 
 Route::get('/auth/{provider}/redirect', [SocialiteController::class, 'redirect'])
