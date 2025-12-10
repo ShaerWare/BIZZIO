55e42d3fb894053366592dcd2ca89c5274efbaac
ShaerWare <artemsher@gmail.com>
2025-12-10 17:38:04 +0000

    фикс меню

--- app/Orchid/PlatformProvider.php
+++ app/Orchid/PlatformProvider.php
@@ -48,45 +48,16 @@ class PlatformProvider extends OrchidServiceProvider
 
             Menu::make(__('Компании'))
                 ->icon('briefcase')
-                ->route('platform.companies.list')
-                ->divider(),
+                ->route('platform.companies.list'),
+                
 
-            Menu::make('Проекты')
+            Menu::make(__('Проекты'))
                 ->icon('rocket')
                 ->route('platform.projects')
-                ->permission('platform.projects'),            
+                //->permission('platform.projects')
+                ->divider(),            
 
-            Menu::make('Get Started')
-                ->icon('bs.book')
-                ->title('Примеры страниц')
-                ->route(config('platform.index')),
-
-            Menu::make('Sample Screen')
-                ->icon('bs.collection')
-                ->route('platform.example')
-                ->badge(fn () => 6),
-
-            Menu::make('Form Elements')
-                ->icon('bs.card-list')
-                ->route('platform.example.fields')
-                ->active('*/examples/form/*'),
-
-            Menu::make('Layouts Overview')
-                ->icon('bs.window-sidebar')
-                ->route('platform.example.layouts'),
-
-            Menu::make('Grid System')
-                ->icon('bs.columns-gap')
-                ->route('platform.example.grid'),
-
-            Menu::make('Charts')
-                ->icon('bs.bar-chart')
-                ->route('platform.example.charts'),
-
-            Menu::make('Cards')
-                ->icon('bs.card-text')
-                ->route('platform.example.cards')
-                ->divider(),
+             
 
             Menu::make('Статус проекта по ТЗ')
                 ->title('Документация')
