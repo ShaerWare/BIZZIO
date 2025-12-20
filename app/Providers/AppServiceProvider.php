d91c2d1c317a4852bba964d519e77427bd662d76
ShaerWare <artemsher@gmail.com>
2025-12-20 17:20:38 +0000

    пофиксил баг с всплывашкой-предупреждением шттпс

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use Laravel\Socialite\Contracts\Factory as SocialiteFactory;
 use Illuminate\Support\Facades\Gate;
 use App\Models\Rfq;
 use App\Policies\RfqPolicy;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -29,9 +30,12 @@ class AppServiceProvider extends ServiceProvider
             $config = $app['config']['services.vk'];
             return $socialite->buildProvider(\SocialiteProviders\VKontakte\Provider::class, $config);
         });
+        /*
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
         }
+            */
+        URL::forceScheme('https');
         // Регистрация Policy для RFQ
         Gate::policy(Rfq::class, RfqPolicy::class);
     }
