70e7e4a16f7e85c365858c70f22e2d8076fa71b5
ShaerWare <artemsher@gmail.com>
2025-12-29 18:12:32 +0000

    меню для мобильникоффф

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -10,84 +10,83 @@
                     </a>
                 </div>
 
-<!-- Navigation Links -->
-<div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-    <!-- Компании -->
-    <x-nav-link :href="route('companies.index')" :active="request()->routeIs('companies.*')">
-        {{ __('Компании') }}
-    </x-nav-link>
-    
-    <!-- Проекты -->
-    <x-nav-link :href="route('projects.index')" :active="request()->routeIs('projects.*')">
-        {{ __('Проекты') }}
-    </x-nav-link>
-    
-    <!-- Тендеры (Dropdown) -->
-                <div class="hidden sm:flex sm:items-center sm:ms-6">
+                <!-- Navigation Links (Desktop) -->
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <!-- Компании -->
+                    <x-nav-link :href="route('companies.index')" :active="request()->routeIs('companies.*')">
+                        {{ __('Компании') }}
+                    </x-nav-link>
+                    
+                    <!-- Проекты -->
+                    <x-nav-link :href="route('projects.index')" :active="request()->routeIs('projects.*')">
+                        {{ __('Проекты') }}
+                    </x-nav-link>
+                    
+                    <!-- Тендеры и Аукционы (Dropdown) -->
+                    <div class="hidden sm:flex sm:items-center sm:ms-6">
+                        <div class="relative" x-data="{ open: false }">
+                            <button @click="open = ! open" 
+                                    class="inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 transition duration-150 ease-in-out border-b-2
+                                        {{ request()->routeIs('rfqs.*', 'auctions.*', 'bids.*', 'invitations.*') ? 'border-indigo-400 text-gray-900 focus:border-indigo-700' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300' }}">
+                                <span>Тендеры и аукционы</span>
+                                <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
+                            </button>
 
-                    <div class="relative" x-data="{ open: false }">
-                        <button @click="open = ! open" 
-                                class="inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 transition duration-150 ease-in-out border-b-2
-                                    {{ request()->routeIs('rfqs.*', 'bids.*', 'invitations.*') ? 'border-indigo-400 text-gray-900 focus:border-indigo-700' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300' }}">
-                            <span>Тендеры и аукционы</span>
-                            <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                            </svg>
-                        </button>
-
-                        <div x-show="open" 
-                            @click.away="open = false"
-                            x-transition
-                            class="absolute z-50 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5"
-                            style="display: none;">
-                            <div class="py-1">
-                                <a href="{{ route('rfqs.index') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                                    Найти тендер
-                                </a>
-                                <a href="{{ route('auctions.index') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                    Найти аукцион
-                </a>
-                                @auth
-                                    @if(auth()->user()->isModeratorOfAnyCompany())
-                                        <a href="{{ route('rfqs.create') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                                            Разместить Запрос котировок
+                            <div x-show="open" 
+                                @click.away="open = false"
+                                x-transition
+                                class="absolute z-50 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5"
+                                style="display: none;">
+                                <div class="py-1">
+                                    <a href="{{ route('rfqs.index') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                                        Найти тендер (RFQ)
+                                    </a>
+                                    <a href="{{ route('auctions.index') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                                        Найти аукцион
+                                    </a>
+                                    @auth
+                                        @if(auth()->user()->isModeratorOfAnyCompany())
+                                            <div class="border-t border-gray-100"></div>
+                                            <a href="{{ route('rfqs.create') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                                                Разместить Запрос котировок
+                                            </a>
+                                            <a href="{{ route('auctions.create') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                                                Разместить аукцион
+                                            </a>
+                                        @endif
+                                        <div class="border-t border-gray-100"></div>
+                                        <a href="{{ route('rfqs.my') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                                            Мои тендеры (RFQ)
                                         </a>
-                                        <a href="{{ route('auctions.create') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                            Разместить аукцион
-                        </a>
-                                    @endif
-                                    <div class="border-t border-gray-100"></div>
-                                    <a href="{{ route('rfqs.my') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                                        Мои тендеры
-                                    </a>
-                                    <a href="{{ route('auctions.my') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                        Мои аукционы
-                    </a>
-                                    <a href="{{ route('bids.my') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                                        Мои заявки  (RFQ)
-                                    </a>
-                                    <a href="{{ route('auctions.bids.my') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                        Мои заявки (Аукционы)
-                                    </a>
-                                    <a href="{{ route('invitations.my') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                                        Мои приглашения (RFQ)
-                                    </a>
-                                     <a href="{{ route('auctions.invitations.my') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                        Мои приглашения (Аукционы)
-                    </a>
-                                    <a href="{{ route('join-requests.index') }}" 
-                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                                        Мои запросы на присоединение
-                                    </a>
-                                @endauth
+                                        <a href="{{ route('auctions.my') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                                            Мои аукционы
+                                        </a>
+                                        <a href="{{ route('bids.my') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                                            Мои заявки (RFQ)
+                                        </a>
+                                        <a href="{{ route('auctions.bids.my') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                                            Мои заявки (Аукционы)
+                                        </a>
+                                        <a href="{{ route('invitations.my') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                                            Мои приглашения (RFQ)
+                                        </a>
+                                        <a href="{{ route('auctions.invitations.my') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                                            Мои приглашения (Аукционы)
+                                        </a>
+                                        <a href="{{ route('join-requests.index') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                                            Мои запросы на присоединение
+                                        </a>
+                                    @endauth
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-            <!-- Settings Dropdown -->
+            <!-- Settings Dropdown (Desktop) -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 @auth
                     <x-dropdown align="right" width="48">
@@ -143,16 +142,82 @@
         </div>
     </div>
 
-    <!-- Responsive Navigation Menu -->
+    <!-- Responsive Navigation Menu (Mobile) -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
+            <!-- Dashboard -->
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
 
+            <!-- Компании -->
             <x-responsive-nav-link :href="route('companies.index')" :active="request()->routeIs('companies.*')">
                 {{ __('Компании') }}
             </x-responsive-nav-link>
+
+            <!-- Проекты -->
+            <x-responsive-nav-link :href="route('projects.index')" :active="request()->routeIs('projects.*')">
+                {{ __('Проекты') }}
+            </x-responsive-nav-link>
+
+            <!-- Тендеры и Аукционы -->
+            <div class="border-t border-gray-200 pt-2 mt-2">
+                <div class="px-4 py-2 text-xs font-semibold text-gray-500 uppercase tracking-wider">
+                    Тендеры и аукционы
+                </div>
+                
+                <x-responsive-nav-link :href="route('rfqs.index')" :active="request()->routeIs('rfqs.index')">
+                    Найти тендер (RFQ)
+                </x-responsive-nav-link>
+                
+                <x-responsive-nav-link :href="route('auctions.index')" :active="request()->routeIs('auctions.index')">
+                    Найти аукцион
+                </x-responsive-nav-link>
+
+                @auth
+                    @if(auth()->user()->isModeratorOfAnyCompany())
+                        <div class="border-t border-gray-100 my-2"></div>
+                        
+                        <x-responsive-nav-link :href="route('rfqs.create')" :active="request()->routeIs('rfqs.create')">
+                            Разместить Запрос котировок
+                        </x-responsive-nav-link>
+                        
+                        <x-responsive-nav-link :href="route('auctions.create')" :active="request()->routeIs('auctions.create')">
+                            Разместить аукцион
+                        </x-responsive-nav-link>
+                    @endif
+
+                    <div class="border-t border-gray-100 my-2"></div>
+                    
+                    <x-responsive-nav-link :href="route('rfqs.my')" :active="request()->routeIs('rfqs.my')">
+                        Мои тендеры (RFQ)
+                    </x-responsive-nav-link>
+                    
+                    <x-responsive-nav-link :href="route('auctions.my')" :active="request()->routeIs('auctions.my')">
+                        Мои аукционы
+                    </x-responsive-nav-link>
+                    
+                    <x-responsive-nav-link :href="route('bids.my')" :active="request()->routeIs('bids.my')">
+                        Мои заявки (RFQ)
+                    </x-responsive-nav-link>
+                    
+                    <x-responsive-nav-link :href="route('auctions.bids.my')" :active="request()->routeIs('auctions.bids.my')">
+                        Мои заявки (Аукционы)
+                    </x-responsive-nav-link>
+                    
+                    <x-responsive-nav-link :href="route('invitations.my')" :active="request()->routeIs('invitations.my')">
+                        Мои приглашения (RFQ)
+                    </x-responsive-nav-link>
+                    
+                    <x-responsive-nav-link :href="route('auctions.invitations.my')" :active="request()->routeIs('auctions.invitations.my')">
+                        Мои приглашения (Аукционы)
+                    </x-responsive-nav-link>
+                    
+                    <x-responsive-nav-link :href="route('join-requests.index')" :active="request()->routeIs('join-requests.index')">
+                        Мои запросы на присоединение
+                    </x-responsive-nav-link>
+                @endauth
+            </div>
         </div>
 
         <!-- Responsive Settings Options -->
