5e1670ddbf7b4542832c8a85698717dd4a8920b0
ShaerWare <artemsher@gmail.com>
2026-01-05 16:25:51 +0000

    РЕГИСТРАЦИЯ КОМАНД В SCHEDULER

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,16 +3,30 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Console\Scheduling\Schedule;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustProxies(at: '*');//
+    ->withMiddleware(function (Middleware $middleware) {
+        //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withSchedule(function (Schedule $schedule) {
+        // Парсинг RSS каждые 15 минут
+        $schedule->command('rss:parse')
+            ->everyFifteenMinutes()
+            ->withoutOverlapping()
+            ->runInBackground();
+
+        // Очистка старых новостей (ежедневно в 02:00 UTC+3)
+        $schedule->command('news:clean-old')
+            ->dailyAt('02:00')
+            ->withoutOverlapping();
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
